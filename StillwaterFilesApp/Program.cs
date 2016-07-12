f5ff825505124e173a7253204e3dfd953754b55b
T.C. Enders <tybEnders@outlook.com>
2016-07-12 15:14:29 -0600

    Initial Commit v.0.1.0
    TODO:
    
    * Make a little more robust in terms of algorithms
    * Get file names from Config file (JSON/XML)
    * Use async/tasks for file I/O
    * add logging!

--- StillwaterFilesApp/Program.cs
+++ StillwaterFilesApp/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
+﻿using System.Linq;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StillwaterFilesApp
 {
     class Program
     {
+        // We'll just start and make this as simple as possible
+        // TODO: Add features like:
+        //       * Robust Logging
+        //       * Macros
+        //       * JSON/XML Config Support
         static void Main(string[] args)
         {
+            // TODO: Make into a config file
+            // TODO: break this functionality into it's own class
+            var lines1 = File.ReadAllLines(@"S:\Ty\tiffs.txt");
+            var lines2 = File.ReadAllLines(@"S:\Ty\files3.txt");
+            var fileNamesFound = from line in lines2
+                                 select Path.GetFileNameWithoutExtension(line);
+            var results = new List<string>();
+            foreach (var line in lines1)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(line);
+                if (!fileNamesFound.Contains(fileName))
+                {
+                    results.Add(line);
+                }
+            }
+
+            File.WriteAllLines(@"S:\Ty\file_diffs.txt", results);
         }
     }
 }
