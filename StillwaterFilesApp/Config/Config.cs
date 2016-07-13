f1fe5aac412b61274b6f05dc19806b92fa45f4cf
T.C. Enders <tybEnders@outlook.com>
2016-07-13 11:01:55 -0600

    Basic Work with Json.Net for config

---
+++ StillwaterFilesApp/Config/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StillwaterFilesApp.Config
+{
+    public class Config
+    {
+        public string InDir { get; set; }
+        public string OutDir { get; set; }
+        public string LogDir { get; set; }
+        public string InFile1 { get; set; }
+        public string InFile2 { get; set; }
+        public string OutFile { get; set; }
+    }
+}
--- StillwaterFilesApp/Program.cs
+++ StillwaterFilesApp/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System.Linq;
 using System.IO;
 using System.Collections.Generic;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json.Linq;
+
 
 namespace StillwaterFilesApp
 {
@@ -15,21 +20,51 @@ namespace StillwaterFilesApp
         {
             // TODO: Make into a config file
             // TODO: break this functionality into it's own class
-            var lines1 = File.ReadAllLines(@"S:\Ty\tiffs.txt");
-            var lines2 = File.ReadAllLines(@"S:\Ty\files3.txt");
-            var fileNamesFound = from line in lines2
+            //var lines1 = File.ReadAllLines(@"S:\Ty\tiffs.txt");
+            //var lines2 = File.ReadAllLines(@"S:\Ty\files3.txt");
+            //var fileNamesFound = from line in lines2
+            //                     select Path.GetFileNameWithoutExtension(line);
+            //var results = new List<string>();
+            //foreach (var line in lines1)
+            //{
+            //    var fileName = Path.GetFileNameWithoutExtension(line);
+            //    if (!fileNamesFound.Contains(fileName))
+            //    {
+            //        results.Add(line);
+            //    }
+            //}
+
+            //File.WriteAllLines(@"S:\Ty\file_diffs.txt", results);
+
+            var jsonString = string.Join("", File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "config.json")));
+
+            JObject json = JObject.Parse(jsonString);
+            var config = JsonConvert.DeserializeObject<Config.Config>(json.ToString());
+            var lines = new Dictionary<string, List<string>>();
+            string[] paths = { Path.Combine(config.InDir, config.InFile1), Path.Combine(config.InDir, config.InFile2) };
+            foreach (var path in paths)
+            {
+                lines.Add(path, new List<string>(File.ReadAllLines(path)));
+            }
+
+            var sorted = from pair in lines
+                         orderby pair.Value.Count() descending
+                         select pair;
+
+            var fileNamesFound = from line in sorted.Last().Value
                                  select Path.GetFileNameWithoutExtension(line);
-            var results = new List<string>();
-            foreach (var line in lines1)
+
+            var diffs = new List<string>();
+            foreach (var line in sorted.First().Value)
             {
                 var fileName = Path.GetFileNameWithoutExtension(line);
                 if (!fileNamesFound.Contains(fileName))
                 {
-                    results.Add(line);
+                    diffs.Add(line);
                 }
             }
 
-            File.WriteAllLines(@"S:\Ty\file_diffs.txt", results);
+            File.WriteAllLines(Path.Combine(config.OutDir, config.OutFile), diffs);
         }
     }
 }
